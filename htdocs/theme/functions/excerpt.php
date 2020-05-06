ff5b3acb08409536a9de56aaee38756048dbca7d
TakafumiMiyake <miyake@t-mwc.com>
2020-05-06 10:07:16 +0900

    Update title.php & excerpt.php

--- htdocs/theme/functions/excerpt.php
+++ htdocs/theme/functions/excerpt.php
@@ -1,17 +1,43 @@
 <?php
-/***********************************************************
+/**
 * 抜粋文字数制限
-***********************************************************/
-function show_limit_excerpt($limit = 40) { 
+*/
+function limit_excerpt($limit = 40) { 
   global $post;
   $excerpt = get_the_excerpt( $post->ID );
   
   if( mb_strlen( $excerpt ) > $limit) {
     $excerpt= mb_substr( $excerpt , 0 , $limit ) ;
-    $show_excerpt = $excerpt. '･･･' ;
+    $show_excerpt = $excerpt. '…' ;
   } else {
     $show_excerpt = $excerpt;
   }
 
-  echo $show_excerpt;
+  return $show_excerpt;
+}
+
+/**
+* エディタがなく、カスタムフィールドで文章を作っている場合
+*/
+function limit_custom_excerpt($limit = 40) {
+  if( have_rows('content') ){
+    while ( have_rows('content') ){
+      the_row();
+
+      if( get_row_layout() == 'content_text' ) {
+        $excerpt = get_sub_field('content_text_text');
+        $excerpt = strip_tags( $excerpt ); // タグ除去
+        break;
+      }
+    }
+  }
+
+  if( $excerpt && mb_strlen( $excerpt ) > $limit) {
+    $excerpt= mb_substr( $excerpt , 0 , $limit ) ;
+    $show_excerpt = $excerpt. '…' ;
+  } else {
+    $show_excerpt = $excerpt;
+  }
+
+  return $show_excerpt;
 }
--- htdocs/theme/functions/title.php
+++ htdocs/theme/functions/title.php
@@ -1,17 +1,17 @@
 <?php
-/***********************************************************
+/**
 * タイトル文字数制限
-***********************************************************/
-function show_limit_title($limit = 20) {
+*/
+function limit_title($limit = 20) {
   global $post;
   $title = $post->post_title;
-  
+
   if( mb_strlen( $title ) > $limit) {
     $title= mb_substr( $title , 0 , $limit ) ;
-    $show_title = $title. '･･･' ;
+    $show_title = $title. '…' ;
   } else {
     $show_title = $title;
   }
 
-  echo $show_title;
+  return $show_title;
 }
