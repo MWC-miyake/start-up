8866554c4562b3663aa1b5dc2a9defb55493c6f5
TakafumiMiyake <miyake@t-mwc.com>
2020-06-05 16:51:26 +0900

    update excerpt.php

--- htdocs/theme/functions/excerpt.php
+++ htdocs/theme/functions/excerpt.php
@@ -2,12 +2,24 @@
 /**
 * 抜粋文字数制限
 */
-function limit_excerpt($limit = 40) { 
+function limit_excerpt( $limit = 40 ) {
   global $post;
-  $excerpt = get_the_excerpt( $post->ID );
-  
+
+  // 「抜粋」フィールドに内容があるかの判定
+  $content = $post->post_excerpt;
+  if( $content ) {
+    $excerpt = $content;
+  } else {
+    $excerpt = get_the_content( $post->ID );
+    $excerpt = strip_shortcodes( $excerpt );
+    $excerpt = strip_tags( $excerpt );
+    $excerpt = str_replace( '&nbsp;', '', $excerpt );
+    $excerpt = preg_replace( '/\n+/', '', $excerpt);
+  }
+
+  // 設定文字数以上ならトリミング
   if( mb_strlen( $excerpt ) > $limit) {
-    $excerpt= mb_substr( $excerpt , 0 , $limit ) ;
+    $excerpt = mb_substr( $excerpt , 0 , $limit ) ;
     $show_excerpt = $excerpt. '…' ;
   } else {
     $show_excerpt = $excerpt;
@@ -18,20 +30,25 @@ function limit_excerpt($limit = 40) {
 
 /**
 * エディタがなく、カスタムフィールドで文章を作っている場合
+* 中身はカスタムフィールドの構造により適宜修正すること
 */
 function limit_custom_excerpt($limit = 40) {
+  // カスタムフィールドから内容を取得する処理
   if( have_rows('content') ){
     while ( have_rows('content') ){
       the_row();
 
       if( get_row_layout() == 'content_text' ) {
         $excerpt = get_sub_field('content_text_text');
-        $excerpt = strip_tags( $excerpt ); // タグ除去
+        $excerpt = strip_tags( $excerpt );
+        $excerpt = str_replace( '&nbsp;', '', $excerpt );
+        $excerpt = preg_replace( '/\n+/', '\n', $excerpt);
         break;
       }
     }
   }
 
+  // 設定文字数以上ならトリミング
   if( $excerpt && mb_strlen( $excerpt ) > $limit) {
     $excerpt= mb_substr( $excerpt , 0 , $limit ) ;
     $show_excerpt = $excerpt. '…' ;
